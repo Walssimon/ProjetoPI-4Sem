3f60f754e380a7a8f3c52033a05d0a5ec669421f
Eric <ets1603@gmail.com>
2025-11-21 00:16:57 -0300

    Adicionar controllers registro user e login user

--- src/main/java/com/curso/boot/catalogoFilmes/web/controller/UsuarioController.java
+++ src/main/java/com/curso/boot/catalogoFilmes/web/controller/UsuarioController.java
@@ -1,12 +1,18 @@
 package com.curso.boot.catalogoFilmes.web.controller;
 
+import com.curso.boot.catalogoFilmes.dao.UsuarioDaoImpl;
+import com.curso.boot.catalogoFilmes.domain.Usuario;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/usuario")
 public class UsuarioController {
+
+    @Autowired
+    private UsuarioDaoImpl usuarioDao;
 
     @GetMapping("/userPage")
     public String UserPage(){
@@ -26,6 +32,32 @@ public class UsuarioController {
     @GetMapping("/registro")
     public String Registrar(){
         return "usuario/registro";
+    }
+    @PostMapping("/registro")
+    public String RegistrarUser(@ModelAttribute Usuario usuario, @RequestParam("rpsenha") String repetirSenha){
+        usuarioDao.save(usuario);
+        return "redirect:/usuario/login";
+    }
+
+    @PostMapping("/login")
+    public String login(
+            @RequestParam String email,
+            @RequestParam String senha,
+            HttpSession session
+    ) {
+
+        Usuario usuario = usuarioDao.findByEmailAndSenha(email, senha);
+
+        if (usuario != null) {
+            session.setAttribute("usuarioLogado", usuario);
+//            Usuario usuarioteste = (Usuario) session.getAttribute("usuarioLogado");
+//            String nome = usuarioteste.getNome();
+//
+//            System.out.println("Nome do usuário logado: " + nome);
+            return "redirect:/home";
+        }
+
+        return "redirect:/usuario/login";
     }
 }
 
