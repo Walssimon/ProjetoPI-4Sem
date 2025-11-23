9ed22654adf8f07e195f1c682565537cf4afa769
Vinicius <vinexply21@gmail.com>
2025-11-23 06:32:38 -0300

    CadastroAdmController.java foi alterado o cadnewfilm e adicionado um postMapping.
    cadNewFilme.html foi alterado somente o action
    
    DataInitializer foi adicionado os generos
    
    Foi criado um FindByNome no GeneroDao e GeneroDaoImpl

--- src/main/java/com/curso/boot/catalogoFilmes/dao/GeneroDaoImpl.java
+++ src/main/java/com/curso/boot/catalogoFilmes/dao/GeneroDaoImpl.java
@@ -5,4 +5,8 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class GeneroDaoImpl extends AbstractDao<Genero, Long> implements GeneroDao {
+    @Override
+    public Genero findByNome(String genero) {
+        return null;
+    }
 }
--- src/main/java/com/curso/boot/catalogoFilmes/web/controller/CadastroAdmController.java
+++ src/main/java/com/curso/boot/catalogoFilmes/web/controller/CadastroAdmController.java
@@ -1,10 +1,11 @@
 
 package com.curso.boot.catalogoFilmes.web.controller;
 import com.curso.boot.catalogoFilmes.dao.AtorDaoImpl;
-import com.curso.boot.catalogoFilmes.domain.Ator;
-import com.curso.boot.catalogoFilmes.domain.Filme;
+import com.curso.boot.catalogoFilmes.domain.*;
 import com.curso.boot.catalogoFilmes.service.FilmeService;
 import com.curso.boot.catalogoFilmes.domain.Filme;
+import com.curso.boot.catalogoFilmes.service.GeneroFilmeService;
+import com.curso.boot.catalogoFilmes.service.GeneroService;
 import com.curso.boot.catalogoFilmes.service.GerarImagemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.ui.Model;
 
+import java.io.IOException;
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -22,6 +25,10 @@ public class CadastroAdmController {
     private GerarImagemService gerarImagem;
     @Autowired
     private FilmeService filmeService;
+    @Autowired
+    private GeneroService generoService;
+    @Autowired
+    private GeneroFilmeService generoFilmeService;
 
     @Autowired
     private AtorDaoImpl atorDao;
@@ -38,7 +45,8 @@ public class CadastroAdmController {
     }
 
     @GetMapping("/cadNewFilme")
-    public String CadNewFilme(){
+    public String CadNewFilme(Model model){
+        model.addAttribute("generos", generoService.findAll());
         return "cadastroAdm/cadNewFilme";
     }
 
@@ -64,4 +72,39 @@ public class CadastroAdmController {
         return "redirect:/cadastroAdm/addActorPage";
     }
 
+    @PostMapping("/cadNewFilme")
+    public String salvarFilme(@RequestParam String nomeFilme,
+                              @RequestParam String descricao,
+                              @RequestParam int duracao,
+                              @RequestParam String dataLancamento,
+                              @RequestParam Double avaliacao,
+                              @RequestParam Integer classificacaoIndicativa,
+                              @RequestParam Long generoId,
+                              @RequestParam("imagem") MultipartFile imagem,
+                              @RequestParam("banner") MultipartFile banner) throws IOException {
+
+        Filme filme = new Filme();
+        filme.setNomeFilme(nomeFilme);
+        filme.setDescricao(descricao);
+        filme.setDuracao(duracao);
+        filme.setDataLancamento(LocalDate.parse(dataLancamento));
+        filme.setAvaliacao(avaliacao);
+        filme.setClassificacaoIndicativa(classificacaoIndicativa);
+
+        filme.setDados(imagem.getBytes());
+        filme.setDadosBanner(banner.getBytes());
+
+        filmeService.save(filme);
+
+        // --- cadastrar na genero_filme ---
+        Genero genero = generoService.findById(generoId);
+
+        GeneroFilme gf = new GeneroFilme();
+        gf.setFilme(filme);
+        gf.setGenero(genero);
+
+        generoFilmeService.save(gf);
+
+        return "redirect:/cadastroAdm/index";
+    }
 }
